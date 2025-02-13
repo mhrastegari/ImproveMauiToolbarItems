5b297453cfc7b7ebebc32ee2a0ebee374266903c
mhrastegari <mh.rastegari@outlook.com>
2025-02-13 17:39:18 +0330

    re-add toolbar items when theme changes

--- ImproveMauiToolbarItems/Platforms/iOS/Handlers/MenuBarContentPageHandler.cs
+++ ImproveMauiToolbarItems/Platforms/iOS/Handlers/MenuBarContentPageHandler.cs
@@ -18,13 +18,13 @@ namespace ImproveMauiToolbarItems.Handlers
         protected override void ConnectHandler(ContentView platformView)
         {
             base.ConnectHandler(platformView);
-            if (ThisPage is null)
-                return;
+            if (ThisPage is null) return;
 
             ThisPage.Loaded += ContentPage_Loaded;
             ThisPage.NavigatedTo += ContentPage_NavigatedTo;
             ThisPage.Appearing += ContentPage_Appearing;
             App.Instance.AppResumed += App_Resumed;
+            Microsoft.Maui.Controls.Application.Current!.RequestedThemeChanged += OnThemeChanged;
         }
 
         protected override void DisconnectHandler(ContentView platformView)
@@ -32,6 +32,8 @@ namespace ImproveMauiToolbarItems.Handlers
             ThisPage!.Loaded -= ContentPage_Loaded;
             ThisPage!.Appearing -= ContentPage_Appearing;
             App.Instance.AppResumed -= App_Resumed;
+            Microsoft.Maui.Controls.Application.Current!.RequestedThemeChanged -= OnThemeChanged;
+
             base.DisconnectHandler(platformView);
         }
 
@@ -51,29 +53,17 @@ namespace ImproveMauiToolbarItems.Handlers
             UpdateItems();
         }
 
-        private void ContentPage_Appearing(object? sender, EventArgs e)
-        {
-            UpdateItems();
-        }
+        private void ContentPage_Appearing(object? sender, EventArgs e) => UpdateItems();
 
-        private void ContentPage_NavigatedTo(object? sender, NavigatedToEventArgs e)
-        {
-            UpdateItems();
-        }
+        private void ContentPage_NavigatedTo(object? sender, NavigatedToEventArgs e) => UpdateItems();
 
-        private void App_Resumed(object? sender, EventArgs e)
-        {
-            // When app is resumed, ToolbarItems are re-added
-            UpdateItems();
-        }
+        private void App_Resumed(object? sender, EventArgs e) => UpdateItems();
+
+        private void OnThemeChanged(object? sender, AppThemeChangedEventArgs e) => UpdateItems();
 
         private void UpdateItems()
         {
-            if (ThisPage is null)
-                return;
-
-            if (this is not IPlatformViewHandler pvh)
-                return;
+            if (ThisPage is null || this is not IPlatformViewHandler pvh) return;
 
             if (pvh.ViewController?.ParentViewController?.NavigationItem is { } navItem)
                 UpdateToolbarItems(ThisPage, navItem);
@@ -90,19 +80,16 @@ namespace ImproveMauiToolbarItems.Handlers
 
         private void UpdateToolbarItems(ContentPage contentPage, UINavigationItem navigationItem)
         {
-            if (contentPage.ToolbarItems is null || contentPage.ToolbarItems.Count == 0)
-                return;
+            if (contentPage.ToolbarItems is null || contentPage.ToolbarItems.Count is 0) return;
 
             var rightBarItems = new List<UIBarButtonItem>();
 
-            // Get primary items
             foreach (var item in contentPage.ToolbarItems)
             {
                 if (item.Order != ToolbarItemOrder.Secondary)
                     rightBarItems.Add(item.ToUIBarButtonItem());
             }
 
-            // Get secondary items
             var secondaryItems = contentPage.ToolbarItems
                 .Where(x => x.Order == ToolbarItemOrder.Secondary)
                 .Select(CreateUIMenuElement)
@@ -110,30 +97,19 @@ namespace ImproveMauiToolbarItems.Handlers
 
             if (secondaryItems is not null || secondaryItems?.Length > 0)
             {
-                // Create a popup UIMenu
                 var menu = UIMenu.Create(string.Empty, null, UIMenuIdentifier.Edit, UIMenuOptions.DisplayInline, secondaryItems);
-
-                // Set ellipsis icon image (can also use UIImage.ActionsImage for a filled ellipsis)
                 var menuButton = new UIBarButtonItem(UIImage.FromBundle("cupertino_ellipsis.png"), menu);
-
-                // Add to final bar items
                 rightBarItems.Add(menuButton);
             }
 
-            // Assign the navigation bar buttons
             navigationItem.RightBarButtonItems = rightBarItems.ToArray();
         }
 
         private static UIMenuElement CreateUIMenuElement(ToolbarItem item)
         {
-            // Create a UIAction for each ToolbarItem
             var imagePath = item.IconImageSource?.ToString();
             var image = string.IsNullOrEmpty(imagePath) ? null : UIImage.FromBundle(imagePath);
-            var action = UIAction.Create(item.Text, image, null, _ =>
-            {
-                item.Command?.Execute(item.CommandParameter);
-            });
-
+            var action = UIAction.Create(item.Text, image, null, _ => item.Command?.Execute(item.CommandParameter));
             return action;
         }
     }
